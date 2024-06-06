eee22a02c2281e0fdb8a628ce7bd4e4d69da38bc
DongKelvin2006 <dongkelvin2006@gmail.com>
2024-06-06 12:17:38 +0200

    Index css

--- PRA_B4_FOTOKIOSK/index.php
+++ PRA_B4_FOTOKIOSK/index.php
@@ -15,9 +15,29 @@
         {
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
+        <div class="box">
+        <h1>Welkom bij de fotokiosk!</h1>
 
-        <h1>Welkom bij de foto boutique!</h1>
-        <img src="public_html/img/logo-big-fill-only.png" alt="logo">
+                <div class="Rrow">
+                    <div class="InfoBox">
+                        <p>Onze Park</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero placeat dolor nemo adipisci, officia nobis, fugit aut quibusdam fugiat fuga rerum repellat rem vero iste natus pariatur, labore et sequi.</p>
+                    </div>
+                <img src="public_html/img/logo-big-fill-only.png" alt="logo">
+            </div>
+
+            <div class="Lrow">
+                <div class="ImgPadding">
+                    <img src="public_html/img/attractie.png" alt="attractie">
+                </div>
+                <div class="InfoBox">
+                    <p>Leuke Attracties:</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores nemo saepe corrupti laboriosam? Quam laborum magnam ullam nihil nulla aperiam nostrum tenetur necessitatibus consequatur iste, vitae dolorem optio. Suscipit!</p>
+                </div>
+            </div>
+
+        </div>
+
     </div>
     <!-- //check 2 -->
 
